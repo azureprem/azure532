7654b5adbe80e0049849f364eafab7b305202ec1
azureprem <azureprem@gmail.com>
2018-05-27 11:12:27 +0530

    Added env setting

--- MyApplication/Controllers/HomeController.cs
+++ MyApplication/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,8 @@ namespace MyApplication.Controllers
     {
         public ActionResult Index()
         {
+            var environment = ConfigurationManager.AppSettings.Get("env");
+            ViewBag.Environment = environment;
             return View();
         }
 
